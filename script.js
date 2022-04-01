5b71ff3489d180b5e2664546fdc4a84fdb12bcfb
mateonav98 <mateonav98@gmail.com>
2022-03-31 17:07:17 -0700

    adding random feature

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseCharacter = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var uppercaseCharacter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numericalCharacter = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacter = ["!","@","#","$","%","^","&","*","+","_","=","-",";"];
-var allCharacters = lowercaseCharacter.concat(uppercaseCharacter, numericalCharacter, specialCharacter)
+var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericalCharacters = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","+","_","=","-",";"];
+var allCharacters = lowercaseCharacters.concat(uppercaseCharacters, numericalCharacters, specialCharacters);
 
 // Write password to the #password input
 function writePassword() {
@@ -19,26 +19,22 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-  var characterCount;
-   prompt("how many characters would you like your password to contain?");
-   if(characterCount<8 || characterCount>128 || !characterCount) {
+function generatePassword() {
+  var characterCount = prompt("how many characters would you like your password to contain?");
+  if(characterCount<8 || characterCount>128 || !characterCount) {
     alert("Password must be between 8-128 characters. Please try again");
-    };
-  var lowercaseOption;
-    confirm("would you like your password to include lowercase letters?");
-  var uppercaseOption;
-    confirm("would you like your password to include uppercase letters?");
-  var numericalOption;
-    confirm("would you like your password to include numbers?");
-  var specialOption;
-    confirm("would you like your password to include special characters (!, @, #, $, ect) ?");
-  if(lowercaseOption===false && uppercaseOption===false && numericalOption===false && specialCharacter===false) {
+    return
+    }
+  var lowercaseOption = confirm("would you like your password to include lowercase letters?");
+  var uppercaseOption = confirm("would you like your password to include uppercase letters?");
+  var numericalOption = confirm("would you like your password to include numbers?");
+  var specialOption = confirm("would you like your password to include special characters (!, @, #, $, ect) ?");
+
+  if(lowercaseOption===false && uppercaseOption===false && numericalOption===false && specialOption===false) {
     alert("No criteria specified. Please try again.");
     };
+
+  var index = Math.floor(Math.random()*allCharacters.length);
+  var randomize = allCharacters[index];
+  return randomize
   };
-
-
-
-
-
